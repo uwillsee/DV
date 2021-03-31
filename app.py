70425ba1464c3d6a12bb0d8c9ef5628861941976
Anastasiia <tagiltsevanastya@gmail.com>
2021-03-31 23:12:06 +0100

    lib

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import graphs
+
 
 import numpy as np
 import pandas as pd
