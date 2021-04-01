41d943d0c27bef06c42a6792dd6238ebb6c4a3f7
uwillsee <tagiltsevanastya@gmail.com>
2021-04-01 10:07:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 import gunicorn
 
 import numpy as np
